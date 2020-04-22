46083d3b5d0522417f1f44ca330bf4554e2be724
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-22 19:42:21 +0200

    Creating GET and POST Express routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { listings } from './listins';
+
 const app = express();
 const port = 9000;
 
-const one = 1;
-const two = 2;
+app.use(bodyParser.json());
 
-app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`));
+app.get('/listings', (_req, res) => {
+  return res.send(listings);
+});
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+  return res.send('failed to delete listing');
+});
 
 app.listen(port);
 
