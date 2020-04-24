ad3ccf217e3769b9eedd7a51cc7cc5f09d14946d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-24 10:18:22 +0200

    Setting up Env variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
+require('dotenv').config();
+
 import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './graphql';
-
-const port = 9000;
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
@@ -14,9 +14,12 @@ const mount = async (app: Application) => {
   });
   server.applyMiddleware({ app, path: '/api' });
 
-  app.listen(port);
+  app.listen(process.env.PORT);
 
-  console.log(`[app] http://localhost:${port}`);
+  console.log(`[app] http://localhost:${process.env.PORT}`);
+
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
 };
 
 mount(express());
