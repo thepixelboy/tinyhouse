3186c2ba6bfb14754040aadadcb428a1b2bc4178
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-24 19:46:29 +0200

    Abstracting the type of data from server fetch

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { server } from '../../lib/api';
+import { ListingsData } from './types';
 
 const LISTINGS = `
   query Listings {
@@ -22,7 +23,7 @@ interface IProps {
 
 export const Listings = ({ title }: IProps) => {
   const fetchListings = async () => {
-    const { data } = await server.fetch({ query: LISTINGS });
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
     console.log(data);
   };
   return (
