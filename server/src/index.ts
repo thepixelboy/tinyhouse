12c78d846f6d235ef5479c9944001d8daabf28fc
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-24 11:27:35 +0200

    Modularizing Resolvers

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,8 @@ import { ApolloServer } from 'apollo-server-express';
 import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './graphql';
 
+const port = process.env.PORT;
+
 const mount = async (app: Application) => {
   const db = await connectDatabase();
   const server = new ApolloServer({
@@ -14,12 +16,9 @@ const mount = async (app: Application) => {
   });
   server.applyMiddleware({ app, path: '/api' });
 
-  app.listen(process.env.PORT);
+  app.listen(port);
 
-  console.log(`[app] http://localhost:${process.env.PORT}`);
-
-  const listings = await db.listings.find({}).toArray();
-  console.log(listings);
+  console.log(`[app] http://localhost:${port}`);
 };
 
 mount(express());
