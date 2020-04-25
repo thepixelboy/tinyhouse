ca29d46451dc40b338a0f75f96f182f284fd7ae7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-25 18:16:47 +0200

    Custom useQuery Hook

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { server } from '../../lib/api';
+import React from 'react';
+import { server, useQuery } from '../../lib/api';
 import {
   ListingsData,
   DeleteListingData,
   DeleteListingVariables,
-  Listing,
 } from './types';
 
 const LISTINGS = `
@@ -35,24 +34,16 @@ interface IProps {
 }
 
 export const Listings = ({ title }: IProps) => {
-  const [listings, setListings] = useState<Listing[] | null>(null);
-
-  useEffect(() => {
-    fetchListings();
-  }, []);
-
-  const fetchListings = async () => {
-    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
-    setListings(data.listings);
-  };
+  const { data } = useQuery<ListingsData>(LISTINGS);
 
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>({
       query: DELETE_LISTING,
       variables: { id },
     });
-    fetchListings();
   };
+
+  const listings = data ? data.listings : null;
 
   const listingsList = listings ? (
     <ul>
