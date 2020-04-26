449e6fd91851279b1176322db47bbf5818fa9a4d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-26 17:21:54 +0200

    Creating the Apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import { Listings } from './sections';
 import * as serviceWorker from './serviceWorker';
 
+const client = new ApolloClient({
+  uri: '/api',
+});
+
 render(
   <React.StrictMode>
-    <Listings title='TinyHouse Listings' />
+    <ApolloProvider client={client}>
+      <Listings title='TinyHouse Listings' />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
