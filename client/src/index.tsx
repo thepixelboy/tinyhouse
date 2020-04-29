89aca1381b03d7b0c3addd0f1cfeda90b5a5fe8d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-04-29 11:03:07 +0200

    Executing Login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
@@ -13,6 +13,7 @@ import {
   NotFound,
   User,
 } from './sections';
+import { Viewer } from './lib/types';
 import * as serviceWorker from './serviceWorker';
 import './styles/indexc.css';
 
@@ -20,7 +21,18 @@ const client = new ApolloClient({
   uri: '/api',
 });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  console.log(viewer);
+
   return (
     <Router>
       <Layout id='app'>
@@ -29,7 +41,11 @@ const App = () => {
           <Route exact path='/host' component={Host} />
           <Route exact path='/listing/:id' component={Listing} />
           <Route exact path='/listings/:location?' component={Listings} />
-          <Route exact path='/login' component={Login} />
+          <Route
+            exact
+            path='/login'
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
           <Route exact path='/user/:id' component={User} />
           <Route component={NotFound} />
         </Switch>
--- server/src/lib/api/Google.ts
+++ server/src/lib/api/Google.ts
@@ -22,7 +22,7 @@ export const Google = {
 
     const { data } = await google.people({ version: 'v1', auth }).people.get({
       resourceName: 'people/me',
-      personFields: 'emailAddresses,name,photos',
+      personFields: 'emailAddresses,names,photos',
     });
 
     return { user: data };
