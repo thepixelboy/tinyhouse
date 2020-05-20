8d7130bf72cb26e2d4b8d9720667fe092830722d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-05-20 16:51:00 +0200

    X-CSRF Token

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -26,6 +26,14 @@ import { AppHeaderSkeleton, ErrorBanner } from './lib/components';
 
 const client = new ApolloClient({
   uri: '/api',
+  request: async (operation) => {
+    const token = sessionStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        'X-CSRF-TOKEN': token || '',
+      },
+    });
+  },
 });
 
 const initialViewer: Viewer = {
@@ -42,6 +50,12 @@ const App = () => {
     onCompleted: (data) => {
       if (data && data.logIn) {
         setViewer(data.logIn);
+
+        if (data.logIn.token) {
+          sessionStorage.setItem('token', data.logIn.token);
+        } else {
+          sessionStorage.removeItem('token');
+        }
       }
     },
   });
