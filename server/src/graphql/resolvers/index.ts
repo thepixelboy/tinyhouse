5367136856e9536cebc119e40067e21ce5033ba7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-07-01 18:40:40 +0200

    User GraphQL Fields

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,4 +1,5 @@
 import merge from 'lodash.merge';
+import { userResolvers } from './Users';
 import { viewerResolvers } from './Viewer';
 
-export const resolvers = merge(viewerResolvers);
+export const resolvers = merge(userResolvers, viewerResolvers);
