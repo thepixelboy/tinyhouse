88233e700681719f4cb29b077b88f762fab5cc22
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-07-11 21:17:58 +0200

    The ListingCreateBooking React component

--- client/src/sections/Listing/index.tsx
+++ client/src/sections/Listing/index.tsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { Col, Layout, Row } from 'antd';
+import { Moment } from 'moment';
 import { ErrorBanner, PageSkeleton } from '../../lib/components';
 import { LISTING } from '../../lib/graphql/queries';
 import {
   Listing as ListingData,
   ListingVariables,
 } from '../../lib/graphql/queries/Listing/__generated__/Listing';
-import { ListingDetails, ListingBookings } from './components';
+import {
+  ListingCreateBooking,
+  ListingDetails,
+  ListingBookings,
+} from './components';
 
 interface MatchParams {
   id: string;
@@ -19,6 +24,8 @@ const PAGE_LIMIT = 3;
 
 export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
   const [bookingsPage, setBookingsPage] = useState(1);
+  const [checkInDate, setCheckInDate] = useState<Moment | null>(null);
+  const [checkOutDate, setCheckOutDate] = useState<Moment | null>(null);
 
   const { loading, data, error } = useQuery<ListingData, ListingVariables>(
     LISTING,
@@ -64,12 +71,25 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
     />
   ) : null;
 
+  const listingCreateBookingElement = listing ? (
+    <ListingCreateBooking
+      price={listing.price}
+      checkInDate={checkInDate}
+      checkOutDate={checkOutDate}
+      setCheckInDate={setCheckInDate}
+      setCheckOutDate={setCheckOutDate}
+    />
+  ) : null;
+
   return (
     <Content className='listings'>
       <Row gutter={24} justify='space-between'>
         <Col xs={24} lg={14}>
           {listingDetailsElement}
           {listingBookingsElement}
+        </Col>
+        <Col xs={24} lg={10}>
+          {listingCreateBookingElement}
         </Col>
       </Row>
     </Content>
