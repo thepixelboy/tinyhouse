c85f8ca6be1559474c9a934443a76f2fa376380a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-07-11 16:23:42 +0200

    Querying for listing data

---
+++ client/src/lib/graphql/queries/Listing/index.ts
@@ -0,0 +1,36 @@
+import { gql } from 'apollo-boost';
+
+export const LISTING = gql`
+  query Listing($id: ID!, $bookingsPage: Int!, $limit: Int!) {
+    listing(id: $id) {
+      id
+      title
+      description
+      image
+      host {
+        id
+        name
+        avatar
+        hasWallet
+      }
+      type
+      address
+      city
+      bookings(limit: $limit, page: $bookingsPage) {
+        total
+        result {
+          tenant {
+            id
+            name
+            avatar
+          }
+          checkIn
+          checkOut
+        }
+      }
+      bookingsIndex
+      price
+      numOfGuests
+    }
+  }
+`;
