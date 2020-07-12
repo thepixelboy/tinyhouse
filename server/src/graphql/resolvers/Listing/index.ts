235cf96e1f6166547fd165ac2f5c3888d83d7ad2
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-07-12 12:57:20 +0200

    Building the Listings Resolvers

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -4,16 +4,19 @@ import { ObjectId } from 'mongodb';
 import { Database, Listing, User } from '../../../lib/types';
 import { authorize } from '../../../lib/utils';
 import {
+  ListingArgs,
   ListingsArgs,
   ListingBookingsArgs,
   ListingBookingsData,
+  ListingsData,
+  ListingsFilter,
 } from './types';
 
 export const listingResolvers: IResolvers = {
   Query: {
     listing: async (
       _root: undefined,
-      { id }: ListingsArgs,
+      { id }: ListingArgs,
       { db, req }: { db: Database; req: Request }
     ): Promise<Listing> => {
       try {
@@ -32,8 +35,37 @@ export const listingResolvers: IResolvers = {
         throw new Error(`Falied to query listing: ${error}`);
       }
     },
-    listings: () => {
-      return 'Query.listings';
+    listings: async (
+      _root: undefined,
+      { filter, limit, page }: ListingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingsData> => {
+      try {
+        const data: ListingsData = {
+          total: 0,
+          result: [],
+        };
+
+        let cursor = await db.listings.find({});
+
+        if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          cursor = cursor.sort({ price: 1 }); // 1: Ascending. Order price LOW to HIGH
+        }
+
+        if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          cursor = cursor.sort({ price: -1 }); // -1: Descending. Order price LOW to HIGH
+        }
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query listings: $(error)`);
+      }
     },
   },
   Listing: {
