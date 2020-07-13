d92c511ac8ca6b98565bf4ef8bb1ead50c803d93
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-07-13 13:48:49 +0200

    Displaying the highest-priced listings in the Homepage

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
 import { Col, Layout, Row, Typography } from 'antd';
+import { LISTINGS } from '../../lib/graphql/queries';
+import {
+  Listings as ListingsData,
+  ListingsVariables,
+} from '../../lib/graphql/queries/Listings/__generated__/Listings';
+import { ListingsFilter } from '../../lib/graphql/globalTypes';
 import { displayErrorMessage } from '../../lib/utils';
-import { HomeHero } from './components';
+import { HomeHero, HomeListings, HomeListingsSkeleton } from './components';
 
 import mapBackground from './assets/map-background.jpg';
 import sanFranciscoImage from './assets/san-fransisco.jpg';
@@ -11,7 +18,21 @@ import cancunImage from './assets/cancun.jpg';
 const { Content } = Layout;
 const { Paragraph, Title } = Typography;
 
+const PAGE_LIMIT = 4;
+const PAGE_NUMBER = 1;
+
 export const Home = ({ history }: RouteComponentProps) => {
+  const { loading, data } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        filter: ListingsFilter.PRICE_HIGH_TO_LOW,
+        limit: PAGE_LIMIT,
+        page: PAGE_NUMBER,
+      },
+    }
+  );
+
   const onSearch = (value: string) => {
     const trimmedValue = value.trim();
 
@@ -20,6 +41,23 @@ export const Home = ({ history }: RouteComponentProps) => {
     } else {
       displayErrorMessage('Please enter a valid search!');
     }
+  };
+
+  const renderListingsSection = () => {
+    if (loading) {
+      return <HomeListingsSkeleton />;
+    }
+
+    if (data) {
+      return (
+        <HomeListings
+          title='Premium Listings'
+          listings={data.listings.result}
+        />
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -44,6 +82,8 @@ export const Home = ({ history }: RouteComponentProps) => {
           Popular listings in the United States
         </Link>
       </div>
+
+      {renderListingsSection()}
 
       <div className='home__listings'>
         <Title level={4} className='home__listings-title'>
