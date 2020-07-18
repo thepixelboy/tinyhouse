234ed9a5ae28ed4dff0fef80f52c139cb0c75dc7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-07-18 20:27:03 +0200

    Searching for listings from the App Header

--- client/src/sections/AppHeader/index.tsx
+++ client/src/sections/AppHeader/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Layout } from 'antd';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Input, Layout } from 'antd';
+import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 import { MenuItems } from './components/';
-import logo from './assets/tinyhouse-logo.png';
 import { Viewer } from '../../lib/types';
+import { displayErrorMessage } from '../../lib/utils';
+
+import logo from './assets/tinyhouse-logo.png';
 
 interface Props {
   viewer: Viewer;
@@ -11,20 +13,59 @@ interface Props {
 }
 
 const { Header } = Layout;
+const { Search } = Input;
 
-export const AppHeader = ({ viewer, setViewer }: Props) => {
-  return (
-    <Header className='app-header'>
-      <div className='app-header__logo-search-section'>
-        <div className='app-header__logo'>
-          <Link to='/'>
-            <img src={logo} alt='App logo' />
-          </Link>
+export const AppHeader = withRouter(
+  ({ viewer, setViewer, location, history }: Props & RouteComponentProps) => {
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+      const { pathname } = location;
+      const pathnameSubStrings = pathname.split('/');
+
+      if (!pathname.includes('/listings')) {
+        setSearch('');
+        return;
+      }
+
+      if (pathname.includes('/listings') && pathnameSubStrings.length === 3) {
+        setSearch(pathnameSubStrings[2]);
+        return;
+      }
+    }, [location]);
+
+    const onSearch = (value: string) => {
+      const trimmedValue = value.trim();
+
+      if (trimmedValue) {
+        history.push(`/listings/${trimmedValue}`);
+      } else {
+        displayErrorMessage('Please enter a valid search!');
+      }
+    };
+
+    return (
+      <Header className='app-header'>
+        <div className='app-header__logo-search-section'>
+          <div className='app-header__logo'>
+            <Link to='/'>
+              <img src={logo} alt='App logo' />
+            </Link>
+          </div>
+          <div className='app-header__search-input'>
+            <Search
+              placeholder='Search "San Francisco"'
+              enterButton
+              value={search}
+              onChange={(evt) => setSearch(evt.target.value)}
+              onSearch={onSearch}
+            />
+          </div>
         </div>
-      </div>
-      <div className='app-header__menu-section'>
-        <MenuItems viewer={viewer} setViewer={setViewer} />
-      </div>
-    </Header>
-  );
-};
+        <div className='app-header__menu-section'>
+          <MenuItems viewer={viewer} setViewer={setViewer} />
+        </div>
+      </Header>
+    );
+  }
+);
--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { RouteComponentProps, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { Affix, Layout, List, Typography } from 'antd';
@@ -25,12 +25,14 @@ const { Paragraph, Text, Title } = Typography;
 const PAGE_LIMIT = 8;
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+  const locationRef = useRef(match.params.location);
   const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
   const [page, setPage] = useState(1);
 
   const { loading, data, error } = useQuery<ListingsData, ListingsVariables>(
     LISTINGS,
     {
+      skip: locationRef.current !== match.params.location && page !== 1,
       variables: {
         location: match.params.location,
         filter,
@@ -39,6 +41,11 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
       },
     }
   );
+
+  useEffect(() => {
+    setPage(1);
+    locationRef.current = match.params.location;
+  }, [match.params.location]);
 
   if (loading) {
     return (
