e4978de52ec55f71f1ba84cf517b968b1033cf90
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-07-18 12:14:46 +0200

    Pagination & Filtering in the Listings page

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
-import { Layout, List, Typography } from 'antd';
-import { ListingCard } from '../../lib/components';
+import { Affix, Layout, List, Typography } from 'antd';
+import { ErrorBanner, ListingCard } from '../../lib/components';
 import { LISTINGS } from '../../lib/graphql/queries';
 import {
   Listings as ListingsData,
   ListingsVariables,
 } from '../../lib/graphql/queries/Listings/__generated__/Listings';
 import { ListingsFilter } from '../../lib/graphql/globalTypes';
+import {
+  ListingsFilters,
+  ListingsPagination,
+  ListingsSkeleton,
+} from './components';
 
 interface MatchParams {
   location: string;
@@ -17,32 +22,71 @@ interface MatchParams {
 const { Content } = Layout;
 const { Paragraph, Text, Title } = Typography;
 
-const PAGE_LIMIT = 0;
+const PAGE_LIMIT = 8;
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
-  const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
-    variables: {
-      location: match.params.location,
-      filter: ListingsFilter.PRICE_LOW_TO_HIGH,
-      limit: PAGE_LIMIT,
-      page: 1,
-    },
-  });
+  const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
+  const [page, setPage] = useState(1);
+
+  const { loading, data, error } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        location: match.params.location,
+        filter,
+        limit: PAGE_LIMIT,
+        page,
+      },
+    }
+  );
+
+  if (loading) {
+    return (
+      <Content className='listings'>
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
+
+  if (error) {
+    return (
+      <Content className='listings'>
+        <ErrorBanner
+          description={`
+            We either couldn't find anything matching your search or have encountered an error.
+            If you're searching for a unique location, try searching again with more common keywords.
+          `}
+        />
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
 
   const listings = data ? data.listings : null;
   const listingsRegion = listings ? listings.region : null;
 
   const listingsSectionElement =
     listings && listings.result.length ? (
-      <List
-        grid={{ gutter: 8, xs: 1, sm: 2, lg: 4 }}
-        dataSource={listings.result}
-        renderItem={(listing) => (
-          <List.Item>
-            <ListingCard listing={listing} />
-          </List.Item>
-        )}
-      />
+      <div>
+        <Affix offsetTop={64}>
+          <ListingsPagination
+            total={listings.total}
+            page={page}
+            limit={PAGE_LIMIT}
+            setPage={setPage}
+          />
+          <ListingsFilters filter={filter} setFilter={setFilter} />
+        </Affix>
+        <List
+          grid={{ gutter: 8, xs: 1, sm: 2, lg: 4 }}
+          dataSource={listings.result}
+          renderItem={(listing) => (
+            <List.Item>
+              <ListingCard listing={listing} />
+            </List.Item>
+          )}
+        />
+      </div>
     ) : (
       <div>
         <Paragraph>
