0fac8e3974aeab600b03aa15d59dcb68e5587df6
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-08-02 18:28:09 +0200

    Image uploads witth Cloudinary

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -14,7 +14,7 @@ import {
   ListingsFilter,
   ListingsQuery,
 } from './types';
-import { Google } from '../../../lib/api';
+import { Cloudinary, Google } from '../../../lib/api';
 
 const verifyHostListingInput = ({
   title,
@@ -132,9 +132,12 @@ export const listingResolvers: IResolvers = {
         throw new Error('Invalid address input');
       }
 
+      const imageUrl = await Cloudinary.upload(input.image);
+
       const insertResult = await db.listings.insertOne({
         _id: new ObjectId(),
         ...input,
+        image: imageUrl,
         bookings: [],
         bookingsIndex: {},
         country,
