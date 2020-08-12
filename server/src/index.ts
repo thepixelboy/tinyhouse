75c750a96d54ea891498c9d19d034a8c8c89016d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-08-12 16:57:00 +0200

    Deploying with Heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,7 @@
-require('dotenv').config();
-
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import compression from 'compression';
 import { ApolloServer } from 'apollo-server-express';
 import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './graphql';
@@ -14,6 +13,10 @@ const mount = async (app: Application) => {
 
   app.use(bodyParser.json({ limit: '2mb' }));
   app.use(cookieParser(process.env.SECRET));
+  app.use(compression());
+
+  app.use(express.static(`${__dirname}/client`));
+  app.get('/*', (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
 
   const server = new ApolloServer({
     typeDefs,
